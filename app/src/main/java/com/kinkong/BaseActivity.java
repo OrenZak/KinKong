ea2a7fc3bb4b7b69f58c7eddc6cc676165a11329
OrenZak <orenzak24@gmail.com>
2017-12-12 12:23:10 +0200

    splash sceen and tutorial

---
+++ app/src/main/java/com/kinkong/BaseActivity.java
@@ -0,0 +1,24 @@
+package com.kinkong;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class BaseActivity extends AppCompatActivity {
+
+    protected ImageView backButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
