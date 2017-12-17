78e21e6b99d2e5db573bb3548231c301500a4a86
Shay Baz <shay@rounds.com>
2017-12-17 13:05:25 +0200

    KIN_KONG-fix_code_hacks

--- app/src/main/java/com/kinkong/CountDownView.java
+++ app/src/main/java/com/kinkong/CountDownView.java
@@ -33,8 +33,8 @@ public class CountDownView extends LinearLayout {
     ViewSwitcher.ViewFactory factory = (ViewSwitcher.ViewFactory) () -> {
         TextView textView = new TextView(getContext());
         textView.setGravity(Gravity.CENTER);
-        //TODO need to fix this not const
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 34);
+        int fontSize = (int) (getResources().getDimension(R.dimen.count_down_font_size_big) / getResources().getDisplayMetrics().density);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize);
         textView.setTextColor(Color.WHITE);
         textView.setTypeface(Typeface.DEFAULT_BOLD);
         return textView;
