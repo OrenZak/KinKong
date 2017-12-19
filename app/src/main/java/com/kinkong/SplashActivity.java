56d9da79f456ddb05410584e0daf36d9051a43fb
YohayBar <yohay@kik.com>
2017-12-19 12:24:31 +0200

    signin anonymously in splash

--- app/src/main/java/com/kinkong/SplashActivity.java
+++ app/src/main/java/com/kinkong/SplashActivity.java
@@ -2,9 +2,17 @@ package com.kinkong;
 
 import android.content.SharedPreferences;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
@@ -30,10 +38,13 @@ public class SplashActivity extends AppCompatActivity {
     private OutputStream outStream;
 
     private SharedPreferences sharedPreferences;
+    private FirebaseAuth firebaseAuth;
+
 
 
     private static final String MAIN_SHAEREDPREF = "kinkong_sharedpref";
     private static final String FIRST_TIME = "first_time";
+    private static final String TAG = SplashActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +53,43 @@ public class SplashActivity extends AppCompatActivity {
         storageReference = FirebaseStorage.getInstance().getReferenceFromUrl("gs://kinkong-977fc.appspot.com").child("kin_tutorial2.mp4");
         kinTutorialFile = new File(getFilesDir() + File.separator + "kin_tutorial.mp4");
         createAccount();
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        signInAnonymously();
+    }
+
+    private void signInAnonymously() {
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if(currentUser != null){
+            Log.d(TAG, "already signInAnonymously");
+            getFireBaseBasicData();
+        }else {
+            firebaseAuth.signInAnonymously()
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInAnonymously:success");
+                                getFireBaseBasicData();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInAnonymously:failure", task.getException());
+                                Toast.makeText(SplashActivity.this, "Authentication failed. close and reopen app",
+                                        Toast.LENGTH_LONG).show();
+                                Toast.makeText(SplashActivity.this, "Authentication failed. close and reopen app",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void getFireBaseBasicData() {
         FBDatabase.getInstance().cacheBasicData(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -63,9 +111,6 @@ public class SplashActivity extends AppCompatActivity {
 
             }
         });
-
-
-
     }
 
     private void createAccount() {
